c08f25c7ce3ec0a0661db4c7217321a4b9ba6e7a
Mario Jackson <mario@jackson.softwre>
2022-07-27 21:52:40 +0200

    Initial commit

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://jackson.software")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-
-
 }
-
