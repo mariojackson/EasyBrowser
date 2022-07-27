d68a8daf655ad7a019ca1d34f2e02e9bc8542a22
Mario Jackson <mario@jackson.softwre>
2022-07-27 20:30:23 +0200

    Initial Commit

---
+++ EasyBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Mario Jackson on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
