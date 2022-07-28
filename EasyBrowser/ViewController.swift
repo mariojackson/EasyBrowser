33938067be3e4c6703f2c03203db027b95b32fb8
Mario Jackson <mario@jackson.softwre>
2022-07-28 18:52:01 +0200

    Let the user chose a web page to open

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -23,5 +23,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://jackson.software")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    }
+    
+    @objc func openTapped() {
+        let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "jackson.software", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(alertController, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let title = action.title else {
+            return
+        }
+        
+        if let url = URL(string: "https://" + title) {
+            webView.load(URLRequest(url: url))
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 }
